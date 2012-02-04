65a11fe433e4323b0f0d1608043e4a839399cf85
mon_sat <tnk@daijiten.info>
2012-02-04 17:03:33 +0900

    version 1.0.0

---
+++ jquery.opencloser.js
@@ -0,0 +1,54 @@
+;(function($) {
+	$.fn.opencloser = function(options) {
+		// build main options before element iteration
+		var opts = $.extend({}, $.fn.opencloser.defaults, options);
+		opts.debug && debug(this);
+		// iterate and reformat each matched element
+		var heights = {};
+		return this.each(function() {
+			var $this = $(this);
+			var id = $this.attr('id');
+			var $link = opts.$target || $('#' + id + opts.link.surfix);
+			heights[id] = $this.height();
+			if (!$link.length) {
+				return;
+			}
+			$link.click(function(){
+				if ($link.text() == opts.link.closer) {
+					// close
+					$this.animate({height: opts.minHeight});
+					$link.text(opts.link.opener);
+				} else {
+					// open
+					$this.animate({height: heights[$this.attr('id')] + 'px'});
+					$link.text(opts.link.closer);
+				}
+			});
+			if (opts.closeByDefault) {
+				$this.height(opts.minHeight);
+			}
+			$link.show();
+		});
+	};
+	// private function for debugging
+	function debug($obj) {
+		if (window.console && window.console.log) {
+			window.console.log('opencloser selection count: ' + $obj.size());
+		}
+	};
+	// define and expose our format function
+	$.fn.opencloser.methodname = function() {
+	};
+	// plugin defaults
+	$.fn.opencloser.defaults = {
+		link: {
+			surfix: '-opencloser',
+			opener: 'もっと見る',
+			closer: 'もとに戻す'
+		},
+		$target: '',
+		closeByDefault: true,
+		minHeight: '3em',
+		debug: 0
+	};
+})(jQuery);
